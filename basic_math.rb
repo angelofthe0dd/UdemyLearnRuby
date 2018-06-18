6a3bc143464b56585a6d9fa077ddb256d1aa2f33
angelofthe0dd <guymontagf451@gmail.com>
2018-06-18 08:56:28 -0400

    First commit

---
+++ basic_math.rb
@@ -0,0 +1,27 @@
+#Basic math operators
+puts "1 + 4:"
+puts 1 + 4
+puts "10 - 6:"
+puts 10 - 6
+puts "10 * 10:"
+puts 10 * 10
+puts "The '+' oerator is both addition and concatenation!"
+puts ""
+puts "Ruby removes the remainder when performing addition"
+puts "12 / 5:"
+puts 12 / 5
+puts "If you make one number a decimal, ruby will return a decimal remainder:"
+puts "12.0 / 5:"
+puts 12.0 / 5
+puts ""
+puts "Ruby uses a double asterisk '**' to denote exponents"
+puts "Two to the power of four, or 2 ** 4:"
+puts 2 ** 4
+puts ""
+puts "The modulo operator '%' returns the remainder of division"
+puts "14 % 4:"
+puts 14 % 4
+puts "You have to include a zero in front of any number that's less than 1"
+puts "100 / 0.5:"
+puts 100 / 0.5
+
