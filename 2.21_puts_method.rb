b4f950774a7fde151f3601967235864cf53e3c03
Jim <4130359+angelofthe0dd@users.noreply.github.com>
2024-02-10 10:49:25 -0500

    Add files via upload

---
+++ 2.21_puts_method.rb
@@ -0,0 +1,7 @@
+# 2.21_puts_method.rb from Udemy course
+
+puts "Hello World!"
+puts 1 + 2
+# use puts alone as a blank space
+puts
+puts "There's a blank line above me."
---
+++ 2.22_escape_char.rb
@@ -0,0 +1,11 @@
+# 2.22_escape_char.rb
+puts "some text"
+puts
+puts "I'll use the newline character \n"
+puts "to break this sentance into two lines."
+puts
+puts "\tThis line is indented with a tab character."
+puts
+puts "This is how you escape \"quotes\"."
+puts
+puts 'If you enclose a string in single quotes, you don\'t need an esacape character to use "double quotes".'
