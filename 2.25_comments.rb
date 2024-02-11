8771a8227f3552f4098bf20b477e348004ef34f5
Jim <4130359+angelofthe0dd@users.noreply.github.com>
2024-02-10 21:42:00 -0500

    Add files via upload

---
+++ 2.25_comments.rb
@@ -0,0 +1,6 @@
+puts
+puts "Ruby uses the '#' for single-line comments"
+puts "Comments are useful for adding explanations to possibly-confusing code."
+puts "Comments can also be used to troubleshoot code. You can"
+puts "put a comment on a line of code (or lines), to limit what gets ran."
+puts
---
+++ 2.26_multiline_comments.rb
@@ -0,0 +1,11 @@
+puts
+=begin
+This is a multiple line comment.
+Everything between the '=begin and the '=end'
+will be ignored by the compiler.
+=end
+puts "For milti-line comments:"
+puts "You start by putting this: '=begin' on the first line (by itself)."
+puts "Then you skip as many lines as you need for your comments."
+puts "On the last line (by itself) you put: '=end'"
+puts
