ca97b74a375115ab79f22be09cf13a05e5d07a7d
Jim <4130359+angelofthe0dd@users.noreply.github.com>
2024-02-14 11:54:13 -0500

    Create 3.32_variables.rb

---
+++ 3.32_variables.rb
@@ -0,0 +1,14 @@
+# This section is just a bunch of slides.
+puts "This section is just a bunch of slides."
+puts
+puts "A variable is a name we assign to an object in our program."
+puts "We assign a value to a variable using the equal sign '=' operator."
+puts "First the name of the variable, then an equal sign, and then the value to be assigned."
+puts
+puts "These are the rules/best practices for nameing variables in Ruby:"
+puts "(1) variable names should start with an underscore or a lowercase letter."
+puts "(2) variable names can only contain letters, numbers, or underscores."
+puts "(3) variable names cannot contain blank spaces."
+puts "(4) variable names are case-sensitive."
+puts "(5) Ruby has 40+ reserve keywords that should never be used as variable names."
+puts
