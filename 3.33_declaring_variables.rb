cd1d0548c65d2974162c9c4348dd0a2c50e65bb3
Jim <4130359+angelofthe0dd@users.noreply.github.com>
2024-02-16 10:39:37 -0500

    Create 3.33_declaring_variables.rb

---
+++ 3.33_declaring_variables.rb
@@ -0,0 +1,7 @@
+# Learning all about creating variables
+puts "You don't need to have spaces between variables and assigned values but the whitespace makes it more readable."
+puts
+puts "Let's create a variable called 'name' and assign it 'Jim'."
+puts "Also we'll create a variable called 'age' and assign it '53'."
+name = "Jim"
+age = 53
