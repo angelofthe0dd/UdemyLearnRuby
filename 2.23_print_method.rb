d270cb5e22b197e2dbee70901798824b37e8c823
Jim <4130359+angelofthe0dd@users.noreply.github.com>
2024-02-16 20:21:16 -0500

    Update 2.23_print_method.rb

--- 2.23_print_method.rb
+++ 2.23_print_method.rb
@@ -1,3 +1,4 @@
+# 2.23_print_method.rb
 # The keyword 'print' doesn't automatically add a new line like 'puts'
 puts "Unlike 'puts', 'print' does not automatically add a new line at the end of the string."
 puts
